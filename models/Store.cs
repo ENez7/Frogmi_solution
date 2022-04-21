6edbfd4e9e94c1ca4dac204bf64b19f411cf3508
Enrique Nuñez <nenrique0112@gmail.com>
2022-04-20 21:43:19 -0400

    Project base implemented

---
+++ models/Store.cs
@@ -0,0 +1,28 @@
+﻿namespace Frogmi.models;
+
+public class Store
+{
+    public List<Incident> Incidents { get; set; } = new List<Incident>();
+
+    public StoreResponse IncidentStatus(DateTime startDate, DateTime endDate)
+    {
+        StoreResponse response = new StoreResponse();
+        response.MaximunSolution = Int32.MinValue;
+        foreach (var incident in Incidents)
+        {
+            if (!incident.isSolved)
+                response.OpenCases++;
+            else
+            {
+                response.SolvedCases++;
+                var solutionTime = Convert.ToInt32((incident.SolvedAt! - incident.ReportedAt).Value.TotalDays);
+                response.AvgSolveTime += solutionTime;
+                if (solutionTime < response.MaximunSolution)
+                    response.MaximunSolution = solutionTime;
+            }
+        }
+
+        response.AvgSolveTime /= response.SolvedCases;
+        return response;
+    }
+}
---
+++ models/StoreResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Frogmi.models;
+
+public class StoreResponse
+{
+    public int OpenCases { get; set; } = 0;
+    public int SolvedCases { get; set; } = 0;
+    public int AvgSolveTime { get; set; } = 0;
+    public int MaximunSolution { get; set; } = 0;
+}
